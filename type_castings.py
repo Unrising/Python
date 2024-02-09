9d900f20de2c8787c7c03272d880bed208b82572
Michael <michael.picard14@gmail.com>
2024-02-09 16:35:03 +0100

    type_casting

---
+++ type_castings.py
@@ -0,0 +1,18 @@
+#type casting = convert the data type to another data type.
+
+x = 1 # int
+y = 2.0 # float
+z = "3" # str
+a = 10
+
+x = float(x) # change 1 to 1.0
+y = int(y) # change 2.0 to 2
+z = int(z) # change "3" to 3
+a = str(a) # change 10 to "10"
+
+print(x)
+print(y)
+print(z)
+
+print("x is : " + str(x))
+print("y is : " + str(y))
