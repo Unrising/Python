6c498befbde055ac3333bb7ab10ecb76831dbea9
Michael <michael.picard14@gmail.com>
2024-02-09 16:41:35 +0100

    user inputs

---
+++ user_input.py
@@ -0,0 +1,17 @@
+# User input 
+
+name = input("What is you name ? : ") # the user input will be assigned to name
+
+print("Hello " + name)
+
+age = int(input("What is your age ? : "))
+
+age = age + 1 
+
+print("You are : " + str(age) + " years old.") 
+
+grade = float(input("What is your grade ? :"))
+
+grade = grade + 3 
+
+print("This is your grade : " + str(grade))
