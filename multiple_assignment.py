7c0282cdbbe1cd5cd146985f8fa3b4e0127b02d2
Michael <michael.picard14@gmail.com>
2024-02-09 16:23:11 +0100

    multiple assignment

---
+++ multiple_assignment.py
@@ -0,0 +1,8 @@
+# multiple_assignment = assign multiple variables at the same time
+
+name, age, human = "Michael", 24, True
+
+print(name + " is " + str(age) + " and he is " + str(human))
+
+pizza = burger = sandwich = salad = 30
+
