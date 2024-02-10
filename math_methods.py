2153cbb89bbf8f83e8493f2808c42e8bc9305d23
Michael <michael.picard14@gmail.com>
2024-02-10 22:41:36 +0100

    math methods

---
+++ math_methods.py
@@ -0,0 +1,25 @@
+import math 
+
+pi = 3.14 
+
+x = -10
+
+y = 25
+
+print(round(pi)) # round to 3 
+
+print(math.ceil(pi)) # round to 4 
+
+print(math.floor(pi)) 
+
+print(abs(x)) # makes x to 10
+
+print(pow(pi,2)) # power of 2
+
+print(math.sqrt(y)) # square root of 25 => 5
+
+print(max(x,y,pi)) # looks for the max
+
+print(min(x,y,pi)) # looks for the min
+
+
