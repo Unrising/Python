6fee1eda8cd44b1c281274f4b8737670bdfa2ea6
Michael <michael.picard14@gmail.com>
2024-02-10 22:59:35 +0100

    if statement

---
+++ if_statement.py
@@ -0,0 +1,14 @@
+# if statement = a block that will execute if it's condition is true
+# watch out for the order
+
+age = int(input("How old are you ?: "))
+
+if age == 100:
+    print("You are very old")
+elif age >= 18 : 
+    print("You are an adult")
+elif age < 0 :
+    print("You haven't been born yet! ")
+else:
+    print("You are a child")
+
