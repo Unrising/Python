a7c70cd36db47cd87bd5e31da8d662082fe0cd59
Michael <michael.picard14@gmail.com>
2024-03-15 15:27:07 +0100

    nested functions

---
+++ nested_function.py
@@ -0,0 +1,10 @@
+# nested functions calls = function calls inside other function calls
+
+
+# num = input("Enter a whole number: ") # Same thing but fewer lines
+# num = float(num)
+# num = abs(num)
+# num = round(num)
+# print(num)
+
+print(round(abs(float(input("Enter a whole number: ")))))
