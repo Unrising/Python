368de43c53bfb97844304b2a2ecbbf864cbf56db
Michael <michael.picard14@gmail.com>
2024-03-15 14:39:52 +0100

    2D_lists

---
+++ 2D_lists.py
@@ -0,0 +1,17 @@
+# 2D list = a list of lists
+
+drinks = ["coffe","soda","tea"]
+
+dinner = ["cake","pizza","hamburger"]
+
+dessert =["ice-cream","fruit"]
+
+food = [drinks,dinner,dessert]
+
+print(food)
+
+print(food[0]) #print the first list
+
+print(food[0][2]) #print the third element of the first list
+
+print(food[1][2])
