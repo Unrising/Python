816daf4b00b70cc39cd6cc6d17df8999767c56f9
Michael <michael.picard14@gmail.com>
2024-03-15 15:24:42 +0100

    keyword arguements

---
+++ keyword_arguments.py
@@ -0,0 +1,19 @@
+# keyword arguments 
+
+# positional arguments
+
+def hello(first,middle,last):
+    print("Hello " + first + " " + middle + " " + last)
+
+
+hello("Code","Dude","Bro")
+
+
+
+def hello(middle,first,last):
+    print("Hello " + first + " " + middle + " " + last)
+
+
+hello(first="Code",middle="Dude",last="Bro")
+
+
