25a9878d5cccb9511d637f833d620b48e9b2657b
Michael <michael.picard14@gmail.com>
2024-03-15 14:57:48 +0100

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,29 @@
+# dictionary = A changeable , unordered collection of unique key:value pairs 
+# Fast beacause they uses hashing , allow us to access a value quickly
+
+
+capitals = {'USA':'Washington DC',
+            'INDIA':'New Delhi',
+            'CHINA':'Benjing',
+            'RUSSIA':'Moscow'}
+
+
+capitals.update({'Germany':'Berlin'}) # Adds germany
+ 
+capitals.update({'USA':'Las Vegas'}) # Change washington dc to las vegas
+
+capitals.pop('RUSSIA') # Remove RUSSIA
+
+# capitals.clear() # clear the dictionnary
+
+print(capitals['CHINA'])
+
+# print(capitals['Germany']) # We get an error
+print(capitals.get('Germany')) # Prints none
+
+print(capitals.keys()) # Prints out the keys
+print(capitals.values()) # Prints out the values
+print(capitals.items()) # Prints everything
+
+for key,value in capitals.items():
+    print(key,value)
